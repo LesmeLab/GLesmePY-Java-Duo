c00efd6aad5e2b4066bd16bb33768c5a0dc7c2d9
LesmeLab <lesmegustavo95@fpuna.edu.py>
2025-08-16 18:03:37 -0300

    B was here

--- Prueba.java
+++ Prueba.java
@@ -1,6 +1,6 @@
 public class Prueba {
 	public static void main ( String [] args ) {
-		System.out.println ( "Prueba Rama A" );
-		System.out.pritnln ( "prueba rama b" )
+		System.out.println ( "Prueba Rama A (B WAS HERE)" );
+		System.out.pritnln ( "prueba rama b" );
 	}
 }
